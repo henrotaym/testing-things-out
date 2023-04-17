4e594484856290912b016e5c982c35df981b6946
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-04-17 16:07:20 +0200

    :alembic: 🚧

--- src/composables/useToasteo.ts
+++ src/composables/useToasteo.ts
@@ -2,6 +2,7 @@ import { reactive } from "vue";
 import { Toasteo } from "../toasteo";
 
 const container = new Toasteo();
+console.log("creating container");
 
 const useToasteo = () => reactive(container);
 export default useToasteo;

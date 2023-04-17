bfcf3cff608d3fb945387a299f96df6cf81b39bc
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-04-17 17:17:51 +0200

    :alembic: 🚧

--- src/components/ToastsList.vue
+++ src/components/ToastsList.vue
@@ -21,7 +21,7 @@
 <script setup lang="ts">
 import { UnwrapNestedRefs } from "vue";
 import { Toasteo } from "../toasteo";
-defineProps<{
+const props = defineProps<{
   toasteo: UnwrapNestedRefs<Toasteo>;
 }>();
 </script>

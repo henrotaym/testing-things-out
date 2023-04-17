eccbe99f29b959e5b93822059b9118f7bea6125b
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-04-17 15:48:46 +0200

    :wrench: 🚧

--- src/components/ToastsList.vue
+++ src/components/ToastsList.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+    <div>{{ name }} is component props name</div>
     <div>
       Toasteo container is named "{{ toasteo.name }}" and contains
       {{ toasteo.toasts.length }} toasts
     </div>
     <ul>
       <li v-for="toast in toasteo.toasts" :key="toast.uuid">
-        {{ toast.title }}
+        {{ toast.uuid }}
       </li>
     </ul>
+    <button @click="toasteo.addToast('super')">Add toast</button>
   </div>
 </template>
 
